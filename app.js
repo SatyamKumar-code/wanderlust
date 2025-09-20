d8a0ad6c3aba51765d23aa5c0e35550bbb859c08
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-09-20 22:45:46 +0530

    app.js are updat for status code

--- app.js
+++ app.js
@@ -119,8 +119,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode=500, message = "something went wrong!" } = err;
-  res.render("error.ejs", { message });
-  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(port, () => {
