6aebfde7c0ab60967b225703ed4e66682d11ea57
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-09-20 20:32:51 +0530

    Add Error.ejs

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode=500, message = "something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  res.render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 app.listen(port, () => {
