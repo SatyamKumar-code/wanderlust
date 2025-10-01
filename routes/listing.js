10a49d986df6027ed851981a859265f3f9f80963
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-10-01 18:07:44 +0530

    Connecting Login Route

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const Listing = require('../models/listing');
 const wrapAsync = require('../utils/wrapAsync.js'); 
 const ExpressError = require('../utils/ExpressError.js');
 const { listingSchema } = require('../schema.js');
+const { isLoggedIn } = require('../middleware/middleware.js');
 
 
 const validateListing = (req, res, next) => {
@@ -26,7 +27,7 @@ router.get("/", wrapAsync(async (req, res) => {
 
 
 // new Route
-router.get("/new", (req, res) => {
+router.get("/new",isLoggedIn, (req, res) => {
   res.render("listings/new");
 });
 
@@ -42,7 +43,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-router.post("/", validateListing,
+router.post("/",isLoggedIn, validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
@@ -52,7 +53,7 @@ router.post("/", validateListing,
 );
 
 // edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit",isLoggedIn, wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   if(!listing) {
@@ -63,7 +64,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id",isLoggedIn, validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   req.flash("success", "Listing Updated!");
@@ -71,7 +72,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 }));
 
 // Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   req.flash("success", "Listing Deleted!");
