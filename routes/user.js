418d0d3f5d67613ebf5f9daa313360fe271d7368
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-10-01 17:55:13 +0530

    Login user

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get("/signup", (req, res) => {
     res.render("users/signup");
@@ -19,4 +20,18 @@ router.post("/signup", wrapAsync(async ( req, res ) => {
         res.redirect("/signup");
     }
 }));
+
+router.get("/login", ( req, res ) => {
+    res.render("users/login");
+});
+
+router.post("/login", passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+}),
+    async ( req, res ) => {
+        req.flash("success", "Welcome back to wanderlust!");
+        res.redirect("/listings");
+    }
+);
 module.exports = router;
